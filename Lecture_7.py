1bf8201ec9c6419f090bf47549d60358963ca35b
Kevin Bui <kevin.bui100@outlook.com>
2025-09-10 12:18:49 -0700

    Setting Up

--- Lecture_7.py
+++ Lecture_7.py
@@ -2,4 +2,28 @@
 '''
 Bayesian models:
 
+Dataset:
+- Steel faults. Input features = measurements. Output outcome = type of fault 
+
+Type of Machine Learning
+-Supervised
+
+Data split
+-Recommend Training/Test in this case
+
+Model Evaluation
+-Accuracy
+
 '''
+#%%
+import pandas as pd
+from matplotlib import pyplot as plt
+from sklearn.naive_bayes import GaussianNB
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report, f1_score, confusion_matrix
+import seaborn as sns
+
+#Load in the dataset
+df = pd.read_csv("faults.csv")
+print(df.head())
+# %%
