512a1d2b02548a8b5bffd51c31f2c5e84cedcd16
Kevin Bui <kevin.bui100@outlook.com>
2025-09-24 11:54:37 -0700

    Lecture13

--- Lecture12.py
+++ Lecture12.py
@@ -29,3 +29,43 @@ Normalization: comparing distance on different scales can be tricky, normalizati
 Value - minimum/(max-min) (min-max scaling)
 
 '''
+#%%
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+from sklearn.cluster import DBSCAN
+from sklearn.preprocessing import MinMaxScaler
+
+df = pd.read_csv("abalone.csv")
+df = df.iloc[0:100]
+print("Features")
+print(df.columns)
+
+outcome = "Rings"
+features = ["Length", "Diameter", "Height", "Whole weight", "Shucked weight","Viscera weight", "Shell weight"]
+feature_df = df[features]
+
+# Weights
+# plt.hist(df["Shell weight"])
+# plt.scatter(df.index,df["Rings"])
+# plt.show()
+
+#Normalizing the data
+scaler = MinMaxScaler()
+feature_df = scaler.fit_transform(feature_df)
+#print(feature_df[0])
+
+#K Means
+k_clusters = 3
+# clustering = KMeans(n_clusters=k_clusters).fit(feature_df)
+
+#DBScan
+eps = 0.2
+min_samples = 2
+clustering = DBSCAN(eps=eps, min_samples=min_samples).fit(feature_df)
+#plt.scatter(df.index, clustering.labels_)
+plt.scatter(df["Rings"], clustering.labels_)
+plt.show()
+plt.clf
+
+# %%
---
+++ Lecture13.py
@@ -0,0 +1,26 @@
+#Tabular Data Model Exploration
+
+'''
+Inputs:
+Coal
+Diesel
+Natural Gas
+Other
+Residual fuel oil
+temp in C
+Total (emissions)
+
+Outputs:
+Fuel Use Characterization
+*Process Heating: Heating process fluids
+*CHP and or cogeneration process
+*Conventional boiler use
+
+Models:
+Linear regression
+Bayes Classifier
+Decision Trees
+Random forests
+Any boosting or bagging
+SVM
+'''
