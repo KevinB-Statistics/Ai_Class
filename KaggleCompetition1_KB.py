6eaeefd3ee68590279d71adb2f7255580cf9dae7
Kevin Bui <kevin.bui100@outlook.com>
2025-10-03 17:59:16 -0700

    kaggle

--- KaggleCompetition1_KB.py
+++ KaggleCompetition1_KB.py
@@ -4,41 +4,45 @@ Kaggle competition
 
 #%%
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler, PolynomialFeatures
+from sklearn.linear_model import ElasticNetCV
+from sklearn.pipeline import Pipeline
+from sklearn.compose import TransformedTargetRegressor
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score, mean_absolute_error
 medical_train = pd.read_csv("ML_Class/train.csv")
 medical_test = pd.read_csv("ML_Class/test.csv")
 
 medical_train_encoded = pd.get_dummies(medical_train, columns=['sex', 'smoker', 'region'], drop_first=True)
 medical_test_encoded = pd.get_dummies(medical_test, columns=['sex', 'smoker', 'region'], drop_first=True)
 
-y = ["charges"]
-x_train = ['age', 'bmi', 'children', 'sex_male', 'smoker_yes', 'region_northwest', 'region_southeast', 'region_southwest']
+y_train = medical_train_encoded["charges"]
+x_train = medical_train_encoded.drop(columns=["charges"])
+
+#test features
 x_test = medical_test_encoded.drop("ID",axis=1)
-
-
-#Create our dataset - inputs and outcomes
-train_features = medical_train_encoded.drop(y, axis=1)
-train_outcomes = medical_train_encoded[y]
-print(train_features.head())
-print(train_outcomes.head())
+x_test = x_test.reindex(columns=x_test.columns, fill_value = 0)
 
 
 #%%
 
-training_features, test_features, training_outcomes, test_outcomes = train_test_split(train_features,train_outcomes,test_size=0.1)
+training_features, test_features, training_outcomes, test_outcomes = train_test_split(x_train,y,test_size=0.1)
 
 #Repeat the model training process, with training/test set
 linear_regression_model = LinearRegression()
-linear_regression_model.fit(train_features, train_outcomes)
-y_pred = linear_regression_model.predict(x_test)
+linear_regression_model.fit(training_features, training_outcomes)
+y_hat = linear_regression_model.predict(test_features)
+print("R^2:", r2_score(test_outcomes, y_hat))
+print("MAE:", mean_absolute_error(test_outcomes, y_hat))
 
+y_pred = linear_regression_model.predict(x_test)
 # %%
-submission = pd.DataFrame({'ID': pd.Series(medical_test_encoded['ID']), 'charges':pd.Series(y_pred)})
-bro = pd.Series(medical_test_encoded['ID'])
-print(bro)
-# submission.to_csv('submission.csv', index=False)
-# print("Submission file successfully created!")
+submission = pd.DataFrame({
+    'ID': medical_test_encoded['ID'],
+    'charges': y_pred.astype(float)})
+submission.to_csv('submission.csv', index=False)
+print("Submission file successfully created!")
 # %%
--- Lecture16.py
+++ Lecture16.py
@@ -1,4 +1,17 @@
 '''
 Neural Network:
 
+Neuron receives input and goes through a transformation process with internal parameters and gives output
+
+Multi layered perceptron
+Neurons are base unit
+Neurons are arranged in layers
+
+How do inputs and outputs work?
+In a fully connected neural network, each layer gets input from the layer before it, and outputs to the next layer
+The first layer gets input from your actual data
+The last layer output is the overall network output 
+
+Important information - 
+It's called a feed forward network, because the information goes forward layer by layer until you get an output
 '''
