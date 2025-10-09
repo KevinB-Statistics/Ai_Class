f392eafeda8c84127e2fb4a4e3837e242bd999fb
Kevin Bui <kevin.bui100@outlook.com>
2025-10-08 23:55:31 -0700

    Assignemnt3

--- Assignment3_KB.py
+++ Assignment3_KB.py
@@ -1,63 +1,143 @@
 '''
-
-
-Due October 8th, 2025
-
-You are going to explore different models we have discussed in class using the fuel_end_use.csv in the "Datasets" tab of class.
-
-Instructions:
-
-    Try to classify the end use of the energy by the GHG emissions and temperature​
-    Pick 3 models​
-    Try 3 different hyperparameter settings on each. At least one hyperparameter must be a property of the underlying model (like tree size, kernel type, etc) ​
-    Get the overall training and test accuracy for each of the 9 models​ (PLEASE report in a table)
-    Also get the per-class prediction accuracy for each model (per class accuracy or F1 score for each of the three outcome classes)​
-    2-3 page write-up on what models performed best/worst, and conjecture as to why
-
-Deliverables:
-
-    Write-up​ (18 points)
-    Code files (2 points) (But If missing, 0 on the entire project)
-
-Write-Up Instructions:
-
-Reporting section that states:​
-
-    Each model you used​ (2 points)
-    Every hyperparameter you changed on the model and what is in (very brief explanation of what it does/how it's used)​ (4 points)
-    Overall training and test accuracy for each of the 9 overall models​ (2 points)
-    Per-class prediction accuracy or F1 score for each of the 9 models (3 outcome classes per model)​ (2 points)
-
-Conclusion section that states:​
-
-    What model of the 9 was best​ (2 points)
-    What model of the 9 was worst​ (2 points)
-    Conjecture as to why certain models did well or poorly​ (2 points)
-    What you learned from the project​ (2 points)
-    About 2-3 pages in length.
-
-Models you can use:
-
-    Linear Regression (any kind that works, like Lasso, Polynomial, etc)​
-    Bayes Classifier​
-    Decision Trees​
-    Random Forests​
-    Any Boosting or Bagging (AdaBoost, Bagging, Gradient Boost, etc)​
-    SVM (SVC or LinearSVC)
-
-Hyperparameters you can change (including but not limited to):
-
-    How much data you sample​
-    Test/Train Split percentage​
-
-Examples of Model-level hyperparameters – Can find in Sci-Kit learn ​
-
-    Decision Tree: criterion (gini or entropy, etc), max_depth, max_features, max_leaf nodes, etc.​
-    Random Forest: criterion, n_estimators, max_depth, max_features, random_state, etc)​
-    AdaBoost: n_estimators, learning rate, algorithm ​
-    SVMs: Type of Kernel, Kernel function parameters, etc​
-    Many more
-
-In class, we will be going over how to do a lot of the data wrangling for this dataset. Feel free to reference the lecture notes/recording from this class session to complete the assignment.
-
+Assignment 3 - Kevin Bui
 '''
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler, LabelEncoder
+from sklearn.pipeline import Pipeline
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
+from sklearn. metrics import confusion_matrix, f1_score
+
+#Load in the dataset
+df = pd.read_csv("fuel_end_use.csv")
+# print(df.head())
+print(df["END_USE"].unique())
+outcomes = ["Process Heating", "CHP and/or Cogeneration Process", "Conventional Boiler Use"]
+features = ["Coal","Diesel","Natural_gas","Other","Residual_fuel_oil", "Temp_degC", "Total"]
+
+#Convert to numeric
+for feature in features:
+    df[feature] = pd.to_numeric(df[feature], errors = "coerce")
+
+#Drop the bad values
+df = df.dropna()
+
+#Separate data by end use
+ph_df = df.loc[df["END_USE"] == outcomes[0]]
+chp_df = df.loc[df["END_USE"] == outcomes[1]]
+boil_df = df.loc[df["END_USE"] == outcomes[2]]
+
+#Box plot
+def plot_var(variable):
+    plt.boxplot([ph_df[variable], chp_df[variable], boil_df[variable]])
+    plt.title(variable)
+    plt.show()
+    plt.clf()
+
+def prep_dataset(df, features, outcome):
+    feature_df = df[features]
+    outcome_df = df[outcome]
+    #Encode the label
+    encoder = LabelEncoder()
+    outcome_df = encoder.fit_transform(outcome_df)
+    print("Classes")
+    print(encoder.classes_)
+    #Normalize the data
+    scaler = MinMaxScaler()
+    feature_df = scaler.fit_transform(feature_df)
+    #Split into training and test setes
+    train_in, test_in, train_out, test_out = train_test_split(feature_df, outcome_df, test_size=0.2, random_state=123)
+    return train_in, test_in, train_out, test_out
+
+train_in, test_in, train_out, test_out = prep_dataset(df, features,"END_USE")
+print(train_in[0])
+
+# For reporting class names aligned to encoded labels
+encoder_for_reporting = LabelEncoder().fit(df["END_USE"])
+label_order = np.unique(test_out)
+class_names = encoder_for_reporting.inverse_transform(label_order)
+
+
+def run_model(model, train_in, test_in, train_out, test_out):
+    model.fit(train_in, train_out)
+    test_accuracy_score = model.score(test_in, test_out)
+    training_accuracy_score = model.score(train_in, train_out)
+    print(f"Training accuracy {training_accuracy_score}")
+    print(f"Test accuracy {test_accuracy_score}")
+    print("Per Class Accuracy")
+    test_predictions = model.predict(test_in)
+    matrix = confusion_matrix(test_out, test_predictions, labels=label_order)
+    class_accuracies = matrix.diagonal()/matrix.sum(axis=1) #diagnoal is correctly classified individuals (percentage of actually predicted individuals)
+    print(class_accuracies)
+    return training_accuracy_score, test_accuracy_score, class_accuracies, test_predictions
+
+
+######## Decision tree, random forest, SVM #########
+results = []
+
+# Decision trees
+dt_param_list = [
+    {"criterion": "gini", "max_depth": None, "min_samples_leaf": 1, "random_state":123},
+    {"criterion": "entropy", "max_depth": 5, "min_samples_leaf": 2, "random_state":123},
+    {"criterion": "gini", "max_depth": 3, "min_samples_leaf": 1, "random_state":123}
+]
+
+for i, p in enumerate(dt_param_list, 1):
+    model = DecisionTreeClassifier(**p)
+    tr_acc, te_acc, per_class_acc, test_pred = run_model(model, train_in, test_in, train_out, test_out)
+    f1s = f1_score(test_out, test_pred, average=None, labels=label_order)
+    row = {"Model": f"DecisionTree_{i}",
+           "Params": p,
+           "Train_Accuracy": tr_acc,
+           "Test_Accuracy": te_acc}
+    for j, cls in enumerate(class_names):
+        row[f"PerClassAcc[{cls}]"] = per_class_acc[j]
+        row[f"F1[{cls}]"] = f1s[j]
+    results.append(row)
+
+#random forests
+
+rf_param_list = [
+    {"n_estimators":200, "max_depth": None,"max_features":"sqrt", "random_state":123},
+    {"n_estimators":500, "max_depth": 5,"max_features":"sqrt", "random_state":123},
+    {"n_estimators":300, "max_depth": None,"max_features":None, "random_state":123}
+]
+for i, p in enumerate(rf_param_list, 1):
+    model = RandomForestClassifier(**p)
+    tr_acc, te_acc, per_class_acc, test_pred = run_model(model, train_in, test_in, train_out, test_out)
+    f1s = f1_score(test_out, test_pred, average=None, labels=label_order)
+    row = {"Model": f"RandomForrest_{i}",
+           "Params": p,
+           "Train_Accuracy": tr_acc,
+           "Test_Accuracy": te_acc}
+    for j, cls in enumerate(class_names):
+        row[f"PerClassAcc[{cls}]"] = per_class_acc[j]
+        row[f"F1[{cls}]"] = f1s[j]
+    results.append(row)
+
+#SVM
+svm_param_list = [
+    {"kernel": "linear", "C":1.0},
+    {"kernel": "rbf", "C":1.0, "gamma":"scale"},
+    {"kernel": "poly", "C": 1.0, "degree":3, "coef0": 1},
+]
+for i, p in enumerate(svm_param_list, 1):
+    model = SVC(**p)
+    tr_acc, te_acc, per_class_acc, test_pred = run_model(model, train_in, test_in, train_out, test_out)
+    f1s = f1_score(test_out, test_pred, average=None, labels=label_order)
+    row = {"Model": f"SVM_{i}",
+           "Params": p,
+           "Train_Accuracy": tr_acc,
+           "Test_Accuracy": te_acc}
+    for j, cls in enumerate(class_names):
+        row[f"PerClassAcc[{cls}]"] = per_class_acc[j]
+        row[f"F1[{cls}]"] = f1s[j]
+    results.append(row)
+
+#Results table
+results_df = pd.DataFrame(results).sort_values("Test_Accuracy", ascending = False).reset_index(drop=True)
+print(results_df)
+results_df.to_csv("Assignment_3_model_results.csv", index=False)
--- KaggleCompetition1_KB.py
+++ KaggleCompetition1_KB.py
@@ -18,7 +18,6 @@ from sklearn.metrics import r2_score, mean_absolute_error
 medical_train = pd.read_csv("ML_Class/train.csv")
 medical_test = pd.read_csv("ML_Class/test.csv")
 
-
 # encoding
 medical_train_encoded = pd.get_dummies(medical_train, columns=['sex', 'smoker', 'region'], drop_first=True)
 medical_test_encoded = pd.get_dummies(medical_test, columns=['sex', 'smoker', 'region'], drop_first=True)
@@ -31,34 +30,30 @@ x_train = medical_train_encoded.drop(columns=["charges"])
 x_test = medical_test_encoded.drop("ID",axis=1)
 x_test = x_test.reindex(columns=x_test.columns, fill_value = 0)
 
-scaler = StandardScaler()
-numerical_cols = ['age', 'bmi', 'children']
-x_test[numerical_cols] = scaler.fit_transform(x_test[numerical_cols])
+training_features, test_features, training_outcomes, test_outcomes = train_test_split(x_train, y_train, test_size = 0.1)
 
-#%%
+############### OLS Model #################
+linear_regression_model = Pipeline([
+    ("scale", StandardScaler()),
+    ("ols", LinearRegression())
+])
 
-#train/validation split
-training_features, test_features, training_outcomes, test_outcomes = train_test_split(x_train,y,test_size=0.1, random_state=123)
-
-#Repeat the model training process, with training/test set
-linear_regression_model = LinearRegression()
 linear_regression_model.fit(training_features, training_outcomes)
 y_hat = linear_regression_model.predict(test_features)
 print("R^2:", r2_score(test_outcomes, y_hat))
 print("MAE:", mean_absolute_error(test_outcomes, y_hat))
 
-
+# predict on test data
 y_pred = linear_regression_model.predict(x_test)
 
-#%%
-#interaction and scaling with elastic net
+################ Polnomial with ElasticNet and Log-transform Model #########################
 
 model = Pipeline([
     ("poly", PolynomialFeatures(degree=2, include_bias=False)),
     ("scale", StandardScaler()),
     ("enet", ElasticNetCV(
         l1_ratio=[0.1,0.25,0.3,0.5,0.7,0.75,0.9,0.95,1.0],
-        cv=100,
+        cv=10,
         n_jobs=-1,
         max_iter=10000,
         tol=1e-4
@@ -72,7 +67,7 @@ linear_regression_model2=TransformedTargetRegressor(
 )
 
 #fit
-linear_regression_model2.fit(training_features,training_outcomes)
+linear_regression_model2.fit(x_train,y_train)
 
 #validation
 y_hat2 = linear_regression_model2.predict(test_features)
@@ -86,28 +81,3 @@ submission = pd.DataFrame({
     'charges': y_pred2.astype(float)})
 submission.to_csv('submission2.csv', index=False)
 print("Submission file successfully created!")
-# %%
-import numpy as np
-import pandas as pd
-from sklearn.preprocessing import OneHotEncoder
-from sklearn.compose import ColumnTransformer
-from sklearn.pipeline import Pipeline
-from sklearn.linear_model import Ridge
-from sklearn.compose import TransformedTargetRegressor
-from sklearn.metrics import mean_absolute_error
-
-# Set global seed
-SEED = 42
-np.random.seed(SEED)
-
-# load data again
-medical_train = pd.read_csv("ML_Class/train.csv").reset_index(drop=True)
-medical_test  = pd.read_csv("ML_Class/test.csv")
-
-# Add ID to training set because they match
-medical_train["ID"] = np.arange(1, len(medical_train) + 1)
-
-# evaluation by appending true values
-truth = medical_test.merge(
-    medical_train[["ID", "charges"]], on="ID", how="left"
-).rename(columns={"charges": "true_charges"})
